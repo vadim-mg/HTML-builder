be1496d864843f6d1360f13b8c2ec083f742bcc5
vadim-mg <vadikm1981@yandex.ru>
2024-01-20 12:03:26 +0500

    feat: implement copy-directory

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,35 @@
+const fs = require('node:fs/promises');
+const path = require('node:path');
+
+const dirName = '04-copy-directory';
+const srcDirName = 'files';
+const dstDirName = 'files-copy';
+
+const srcDirPath = path.normalize(path.join(dirName, srcDirName));
+const dstDirPath = path.normalize(path.join(dirName, dstDirName));
+
+const copyDir = async (srcPath, dstPath) => {
+  try {
+    await fs.rm(dstPath, {
+      force: true,
+      recursive: true,
+    });
+    await fs.mkdir(dstPath, {
+      recursive: true,
+    });
+    fs.readdir(srcPath, {
+      withFileTypes: true,
+    }).then(async (files) => {
+      for (const file of files) {
+        await fs.copyFile(
+          path.join(file.path, file.name),
+          path.join(dstPath, file.name),
+        );
+      }
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+copyDir(srcDirPath, dstDirPath);
