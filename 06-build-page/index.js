f4875d801bd60d46a3397a1e7374943b549c2830
vadim-mg <vadikm1981@yandex.ru>
2024-01-20 19:56:41 +0500

    feat: implement copy assets

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -14,6 +14,33 @@ const DIST_STYLES_FILE_NAME = 'style.css';
 
 const makePath = (...paths) => path.normalize(path.join(DIR_NAME, ...paths));
 
+const copyDir = async (srcPath, dstPath) => {
+  try {
+    await fs.rm(dstPath, {
+      force: true,
+      recursive: true,
+    });
+    await fs.mkdir(dstPath, {
+      recursive: true,
+    });
+    fs.readdir(srcPath, {
+      withFileTypes: true,
+    }).then(async (files) => {
+      for (const file of files) {
+        const srcFilePath = path.join(file.path, file.name);
+        const dstFilePath = path.join(dstPath, file.name);
+        if (file.isFile()) {
+          await fs.copyFile(srcFilePath, dstFilePath);
+        } else if (file.isDirectory) {
+          await copyDir(srcFilePath, dstFilePath);
+        }
+      }
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
 const bundler = async (distDirName) => {
   const distDirPath = makePath(distDirName);
   const distAssetsPath = makePath(distDirName, ASSETS_DIR_NAME);
@@ -23,6 +50,7 @@ const bundler = async (distDirName) => {
   const srcTemplatesPath = makePath(SRC_TEMPLATES_DIR);
   const srcMainTemplatePath = makePath(SRC_MAIN_TEMPLATE_FILE_NAME);
   const srcStylesPath = makePath(SRC_STYLES_DIR);
+  const srcAssetsPath = makePath(ASSETS_DIR_NAME);
 
   console.log(distDirPath);
   console.log(distAssetsPath);
@@ -31,8 +59,24 @@ const bundler = async (distDirName) => {
   console.log(srcTemplatesPath);
   console.log(srcMainTemplatePath);
   console.log(srcStylesPath);
+  console.log(srcAssetsPath);
+
+  try {
+    /* Create dist folder */
+    await fs.rm(distDirPath, {
+      force: true,
+      recursive: true,
+    });
+    fs.mkdir(distDirPath, { recursive: true });
+
+    /* Copy Assets */
+    await copyDir(srcAssetsPath, distAssetsPath);
 
 
+  } catch (err) {
+    console.error(err);
+    return err;
+  }
 };
 
 bundler(DIST_DIR_NAME);
